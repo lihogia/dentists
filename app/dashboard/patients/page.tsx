10833a7bc47c7f2e6c994c6bedbb46ac9be977a4
Linh Hong <linh.hong.sg@gmail.com>
2024-06-27 16:13:51 +0700

    temporary comment search part

--- app/dashboard/patients/page.tsx
+++ app/dashboard/patients/page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
                 <h1 className={`${lusitana.className} text-2xl`}>Patients</h1>
             </div>
             <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-                <Search placeholder="Search patients..." />
+                {/*<Search placeholder="Search patients..." />*/}
                 <CreatePatient />
             </div>
             <PatientsTable />
