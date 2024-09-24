81e19522f2e60c3cfc23b014151b76a105d66eb6
Linh Hong <linh.hong.sg@gmail.com>
2024-09-24 14:23:14 +0700

    clean middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -13,9 +13,6 @@ const { auth } = NextAuth(authConfig);
 export default auth((req) => {
     const { nextUrl } = req;
     const isLoggedIn = !!req.auth;
-
-    //console.log(`is loggedin: ${isLoggedIn}`);
-
     const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
     const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
     const isAuthRoute = authRoutes.includes(nextUrl.pathname);
@@ -23,27 +20,21 @@ export default auth((req) => {
     if (isApiAuthRoute) { 
         return;
     }
-    
     if (isAuthRoute) {
         if (isLoggedIn) {
             return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
         }
         return;
     }
-
     if (!isLoggedIn && !isPublicRoute) {
         return Response.redirect(new URL("/login", nextUrl));
     }
-
     return;
 })
 
 // Those below will invoke the auth function above
 export const config = {
-    //matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
     matcher: [
-        '/((?!.+\\.[\\w]+$|_next).*)',
-        '/',
-        '/(api|trpc)(.*)',
+        '/((?!.+\\.[\\w]+$|_next).*)','/','/(api|trpc)(.*)',
     ]
 }
