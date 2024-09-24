c7d3b0abff181290cd25d6fb13e6318d7821bb68
Linh Hong <linh.hong.sg@gmail.com>
2024-09-24 16:55:29 +0700

    clean code in middleware to be 1MB only

--- auth.config.ts
+++ auth.config.ts
@@ -1,6 +1,5 @@
 import bcrypt from "bcryptjs";
 import Credentials from "next-auth/providers/credentials";
-import Github from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 import type { NextAuthConfig } from "next-auth";
 
@@ -13,23 +12,14 @@ export default {
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         }),
-        Github({
-            clientId: process.env.GITHUB_CLIENT_ID,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        }),
         Credentials({
             async authorize(credentials) {
                 const validatedFields = LoginSchema.safeParse(credentials);
                 if (validatedFields.success) {
                     const { email, password } = validatedFields.data;
                     const user = await getUserByEmail(email);
-                    /*const user = {
-                        email: 'lilogia@gmail.com',
-                        password: 'abc123'
-                    }*/
                     if (!user || !user.password) return null;
                     const passwordMatch = await bcrypt.compare(password, user.password);
-                    //const passwordMatch = true;
                     if (passwordMatch) return user;
                 }
                 return null;
--- auth.ts
+++ auth.ts
@@ -1,23 +1,19 @@
 import NextAuth, { type DefaultSession } from "next-auth";
 import authConfig from "@/auth.config";
 import { UserRole } from "@prisma/client";
-
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "@/app/lib/data/database";
 import { getUserById } from "@/app/lib/data/authPrisma";
-
 //import { JWT } from "@auth/core/jwt";
 
 export type ExtendedUser = DefaultSession["user"] & {
     role: UserRole;
 };
-
 declare module "next-auth" {
     interface Session {
         user: ExtendedUser;
     }
 }
-
 declare module "@auth/core/jwt" {
     interface JWT {
         role: ExtendedUser["role"];
@@ -38,11 +34,9 @@ export const {
             if (token.role && session.user) {
                 session.user.role = token.role;
             }
-            //console.log({ sessionToken: token, session});
             return session;
         },
         async jwt({ token }) {
-            //console.log(`token : ${token}, ${token.sub}`);
             if (!token.sub) return token;
             const existingUser = await getUserById(token.sub);
             if (!existingUser) return token;
